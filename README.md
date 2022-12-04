31e6b084b3ba940b33f61ee0c72b76e18e451a95
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2022-12-04 16:47:51 +0900

    urls

--- README.md
+++ README.md
@@ -8,4 +8,5 @@
 2. localStorage of files
 3. Drag & drop
 4. set loading spinner while a file is being uploaded
-5. 
+5. Save image download urls with name and password
+6. 
--- src/pages/Download.jsx
+++ src/pages/Download.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import "../stylesheet/_download.scss";
 
 const Download = ({ files, urls, setFiles, setUrls }) => {
-  
   return (
     <div className="download">
       <h2>Download</h2>
-      <div></div>
+      <ul>
+        {urls &&
+          urls.map((url) => {
+            return (
+              <li>
+                <a href={url}>document</a>
+              </li>
+            );
+          })}
+      </ul>
     </div>
   );
 };
